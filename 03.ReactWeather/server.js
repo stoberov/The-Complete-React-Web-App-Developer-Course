1e6b409ede1374d7785ac46fffb36585d98b1d84
Stoyan Berov <stoyan.berov@gmail.com>
2017-05-28 17:50:07 +0300

    feat: prep repo for Heroku

--- 03.ReactWeather/server.js
+++ 03.ReactWeather/server.js
@@ -3,8 +3,20 @@ var express = require('express');
 // Create our app
 var app = express();
 
+// Heroku port or default to 3000 on localhost
+const PORT = process.env.PORT || 3000;
+
+// Redirect HTTPS to HTTP
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-    console.log('Express server is up on port 3000');
+app.listen(PORT, function() {
+    console.log('Express server is up on port ' + PORT);
 });
